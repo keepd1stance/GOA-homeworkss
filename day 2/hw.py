826938654eeeeb856b48c57ff5d404b3805a15ab
keepd1stance <zniniachviligiorgi@gmail.com>
2024-05-20 17:48:15 +0400

    4th commit

--- day 2/hw.py
+++
@@ -1,1 +0,0 @@
-print("that's all i got for now              erekle kilasonia          gio gagnidze          gio kurtishvili            ")
---
+++ day3/lesson3(type2).py
@@ -0,0 +1,31 @@
+from turtle import*
+
+item="phone"
+#   just first variable
+
+print("Your battery is low, please charge.")
+#   comment N1
+
+phone=20
+#   variable value
+
+
+item="charger"
+#   charger xd
+
+print("Phone battery is low, so we have to charge it")
+#   comment N2
+
+print("*charges phone*")
+#   comment N3
+
+
+charger=80
+# variable value N2
+
+
+print(phone+charger)
+#   value + value
+
+print("Yay, now phone is charged!!!")
+#   comment N4
