449785970029d0b205ac6e1362a1a00167a12aa6
keepd1stance <zniniachviligiorgi@gmail.com>
2024-05-25 17:03:03 +0400

    commit

---
+++ day 04/cw/Untitled-1.py
@@ -0,0 +1,25 @@
+age=15
+
+future_age = age + 2 
+
+print(future_age)
+
+
+
+
+
+age=15
+
+
+age = age + 1 
+
+
+print(age)
+
+#variable = Declaration (anu deklaracia qvia cvlads, cvladis sheqmnas)
+#variable value = Initialization (mnishvnelobis saxeli)
+
+age += 2
+age -= 2
+age *= 2
+age /= 2
