99a1a9b31a84a5393fff93a848ff6bbc8022932b
keepd1stance <zniniachviligiorgi@gmail.com>
2024-05-28 17:27:48 +0400

    commit

---
+++ day 05/hw/homework05.py
@@ -0,0 +1,61 @@
+from turtle import*
+#homework N1
+int()
+
+
+
+
+item="g.niniashvili"
+
+
+name="giorgi"
+surname="niniashvili"
+age=14
+adress="varketili, javaxidzis qucha"
+
+
+#homework N2
+
+#bank
+#you have 100$ on your account
+#you got paid on your job
+
+balance=100
+salary=1500
+
+print(salary+balance)
+balance=1600
+
+#you got cut
+cut=400
+print(balance-cut)
+balance=1200
+
+#you decided to play slots
+#you win x4
+win=4
+print(balance*win)
+balance=6400
+
+#you decided to play more and you lost almost everything
+lost=10
+print(balance/lost)
+balance=640
+
+#now you have to pay bills
+food=350
+electricity=300
+water_bill=400
+gas=250
+
+print(balance-(food+electricity+water_bill+gas))
+
+balance=-660
+
+#now your in debt
+#you tell your friend that you have problems and you're in debt. he gives you 300$
+friend=300
+print(balance+friend)
+
+#if YOU dont want to be like this every day, every month and every year dont gamble. 
+
