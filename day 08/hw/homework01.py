188574cca03d2683e6c619a9da3069ac3efbf4a1
keepd1stance <zniniachviligiorgi@gmail.com>
2024-06-09 19:14:39 +0400

    commit

---
+++ day 08/hw/homework01.py
@@ -0,0 +1,15 @@
+num = 3
+
+print(num > 1 and num <= 10)
+print(num <= 1 and num >= 4)
+print(num > 2 and num < 3)
+print(num >= 5 and num > 1)
+print(num < 4 and num <=15)
+
+print(num < 1 or num > 1)
+print(num > 5 or num < 10)
+print(num > 3 or num <= 4)
+print(num < 3 or num > 5) 
+print(num < 4 or num >= 6)
+
+ 
