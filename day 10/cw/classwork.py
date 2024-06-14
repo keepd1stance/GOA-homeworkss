d8cf30d23fedee9f81a0929d40c8d09dc6f7d76f
keepd1stance <zniniachviligiorgi@gmail.com>
2024-06-14 18:24:24 +0400

    commit

---
+++ day 10/cw/classwork.py
@@ -0,0 +1,2 @@
+for i in range(100000000000000):
+    print(str(i) + "." "Dzalian momewona gakvetili")
---
+++ day 10/hw/homework.py
@@ -0,0 +1,91 @@
+#just for loops
+for i in range(10 + 1):
+    print("xachapuri")
+
+print(  )
+
+for i in range(10 + 1):
+    print(i)
+
+print(  )
+
+a=20
+for i in range(a + 1):
+    print(i)
+
+print(  )
+
+
+#if loops
+for i in range(10+2):
+  if i % 2 == 0:
+    print(i)
+
+print(  )
+
+for x in range(100):
+  if x % 10 == 0:
+    print(x)
+
+print(  )
+
+for y in range(500+100):
+  if y % 100 == 0:
+     print(y)
+
+print(  )
+
+
+#if and else loops
+for x in range(100+1):
+  if x % 2 == 0:
+    print(str(x) + " luwia")
+  else:
+     print(str(x) + " kentia")
+
+print(  )
+
+
+#1 kentia, ertis magivrad rom chavwerot luwi ricxvi, dagviwers sworad kentia tu luwia
+for  i in range (1, 20, 44):
+  if i % 2 == 0:
+     print(str(i) + " luwia")
+  else:
+     print(str(i) + " kentia")
+
+print(  )
+
+#sheidzleba tqvat rom plagiatori var da ragaca magram es me kodi gavaswore, ar ewera sworad
+#rac ar unda chawerot, or cifrs shoris naklebi romelicaa imas gamoiyvans terminalshi
+num1 = int(input("please enter the first number: "))
+num2 = int(input("please enter the second number: "))
+sum = 0
+if num1 > num2:
+    big = num1
+    small = num2
+else:
+    small = num1
+    big = num2
+for i in range (small, big):
+    print(small)
+
+print(  )
+
+#rac ar unda chawerot, or cifrs shoris meti romelicaa imas gamoiyvans terminalshi
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+sum = 0
+if num1 > num2:
+    big = num1
+    small = num2
+else:
+    small = num1
+    big = num2
+for i in range (small, big):
+    print(big)
+
+
+
+
+     
+   
